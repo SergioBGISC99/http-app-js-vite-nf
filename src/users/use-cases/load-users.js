d38147824cadbf49b5c0f0bed2644373c78c94d5
Sergio Barreras <sergiobg.isc@gmail.com>
2023-11-14 17:21:31 -0700

    load users by page listo

--- src/users/use-cases/load-users.js
+++
@@ -1,7 +0,0 @@
-export const loadUsers = async (page = 1) => {
-  const url = `${import.meta.env.VITE_BASE_URL}/users?_page=${page}`;
-  const res = await fetch(url);
-  const data = await res.json();
-
-  console.log(data);
-};
