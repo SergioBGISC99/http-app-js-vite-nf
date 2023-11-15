4d6127dd9d87bc8657392a2d8f38ea92297a0774
Sergio Barreras <sergiobg.isc@gmail.com>
2023-11-14 22:50:41 -0700

    botones implementados

--- src/users/store/users-store.js
+++ src/users/store/users-store.js
@@ -13,7 +13,10 @@ const loadNextPage = async () => {
 };
 
 const loadPreviousPage = async () => {
-  throw new Error("No implementado");
+  if (state.currentPage === 1) return;
+  const users = await loadUsersByPage(state.currentPage - 1);
+  state.currentPage -= 1;
+  state.users = users;
 };
 
 const onUserChanger = () => {
