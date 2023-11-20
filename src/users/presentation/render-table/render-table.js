a3bfb4c7114efd4a527905b8e2e724d536dded3c
Sergio Barreras <sergiobg.isc@gmail.com>
2023-11-19 21:52:41 -0700

    obtener información de usuario

--- src/users/presentation/render-table/render-table.js
+++ src/users/presentation/render-table/render-table.js
@@ -1,4 +1,5 @@
 import usersStore from "../../store/users-store";
+import { showModal } from "../render-modal/render-modal";
 import "./render-table.css";
 
 let table;
@@ -23,6 +24,18 @@ const createTable = () => {
 
 /**
  *
+ * @param {MouseEvent} event
+ * @returns
+ */
+const tableSelectListener = (event) => {
+  const element = event.target.closest(".select-user");
+  if (!element) return;
+  const id = element.getAttribute("data-id");
+  showModal(id);
+};
+
+/**
+ *
  * @param {HTMLDivElement} element
  */
 export const renderTable = (element) => {
@@ -30,6 +43,8 @@ export const renderTable = (element) => {
   if (!table) {
     table = createTable();
     element.append(table);
+
+    table.addEventListener("click", tableSelectListener);
   }
 
   let tableHTML = "";
@@ -42,11 +57,11 @@ export const renderTable = (element) => {
         <td>${user.lastName}</td>
         <td>${user.isActive}</td>
         <td>
-            <a href="#/" data-id="${user.id}">✅</a> |
-            <a href="#/" data-id="${user.id}">🗑️</a>
+            <a href="#/" class="select-user" data-id="${user.id}">✅</a> |
+            <a href="#/" class="delete-user" data-id="${user.id}">🗑️</a>
         </td>
     </tr>`;
   });
 
-  table.querySelector('tbody').innerHTML = tableHTML;
+  table.querySelector("tbody").innerHTML = tableHTML;
 };
