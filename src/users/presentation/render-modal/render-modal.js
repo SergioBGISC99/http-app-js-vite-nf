3fb68a611ccf221a02166361ffb7f18ea3e66c11
Sergio Barreras <sergiobg.isc@gmail.com>
2023-11-19 18:34:36 -0700

    comportamiento form

--- src/users/presentation/render-modal/render-modal.js
+++ src/users/presentation/render-modal/render-modal.js
@@ -1,7 +1,15 @@
 import "./render-modal.css";
 import modalHtml from "./render-modal.html?raw";
 
-let modal;
+let modal, form;
+
+export const showModal = () => {
+  modal?.classList.remove("hide-modal");
+};
+
+export const hideModal = () => {
+  modal?.classList.add("hide-modal");
+};
 
 /**
  *
@@ -11,8 +19,18 @@ export const renderModal = (element) => {
   if (modal) return;
   modal = document.createElement("div");
   modal.innerHTML = modalHtml;
-
   modal.className = "modal-container hide-modal";
+
+  form = modal.querySelector("form");
+
+  modal.addEventListener("click", (event) => {
+    if (event.target.className !== "modal-container") return;
+    hideModal();
+  });
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+  });
 
   element.append(modal);
 };
